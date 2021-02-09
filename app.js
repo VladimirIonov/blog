3f3c72703ed7b22d8e6cf18b5a79cf5ffe01406c
VladimirIonov <killer2560@yandex.ru>
2021-02-09 16:57:53 +0300

    Oformlenie info

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ console.log(`App is running on http://localhost:${port}/`);
 
 app.get("/", (request, response) => {
 const file = fs.readFileSync("./data/notes.json");
+
 const model = JSON.parse(file);
 response.render("index", model);
 });
+
 app.get("/info/:id", (request, response) => {
 const id = request.params.id;
 const file = fs.readFileSync("./data/notes.json");
@@ -24,7 +26,41 @@ const notes = model.notes.filter(x => x.id == id);
 if (notes.length < 1) response.redirect("/");
 
 response.render("info", notes[0]);
-}); 
+});
+
+app.get("/admin/edit/:id", (request, response) => {
+const id = request.params.id;
+const file = fs.readFileSync("./data/notes.json");
+const model = JSON.parse(file);
+const notes = model.notes.filter(x => x.id == id);
+
+let note = {};
+if (notes.length > 0) note = notes[0];
+
+response.render("info", note);
+});
+
+app.post("/admin/edit", (request, response) => {
+const editInfo = request.body;
+if (!editInfo) response.redirect("/");
+
+const file = fs.readFileSync("./data/notes.json");
+const model = JSON.parse(file);
+const notes = model.notes.filter(x => x.id == id);
+
+if (notes.length < 1) {
+const note = notes[0];
+
+note.title = editInfo.title;
+note.text = editInfo.text;
+} else {
+note.id = notes[notes.length - 1].id + 1;
+notes.push(note);
+}
+
+response.redirect("/");
+});
+
 app.get("/*", (request, response) => {
 response.redirect("/");
 });
