793cf0da4447e39d6d956c77a63afef2ef83e934
VladimirIonov <killer2560@yandex.ru>
2021-02-10 13:46:36 +0300

    Добавили редактирование новости Добавили добавление новости

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const fs = require("fs");
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 2000;
 
 app.set("view engine", "hbs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
 console.log(`App is running on http://localhost:${port}/`);
@@ -37,30 +40,45 @@ const notes = model.notes.filter(x => x.id == id);
 let note = {};
 if (notes.length > 0) note = notes[0];
 
-response.render("info", note);
+response.render("edit-info", note);
+});
+
+app.get("/admin/add", (request, response) => {
+const note = {};
+
+response.render("edit-info", note);
 });
 
 app.post("/admin/edit", (request, response) => {
 const editInfo = request.body;
 if (!editInfo) response.redirect("/");
 
+editInfo.id = Number(editInfo.id);
+
 const file = fs.readFileSync("./data/notes.json");
 const model = JSON.parse(file);
-const notes = model.notes.filter(x => x.id == id);
-
-if (notes.length < 1) {
-const note = notes[0];
+const notes = model.notes.filter(x => x.id == editInfo.id);
+let note = {};
+if (notes.length > 0) {
+note = notes[0];
 
 note.title = editInfo.title;
 note.text = editInfo.text;
+note.predtext = editInfo.predtext;
 } else {
-note.id = notes[notes.length - 1].id + 1;
-notes.push(note);
+note.id = Math.floor(Math.random()*1000000 + 1);
+note.title = editInfo.title;
+note.predtext = editInfo.predtext;
+
+model.notes.push(note);
 }
+
+const json = JSON.stringify(model);
+fs.writeFileSync("./data/notes.json", json);
 
 response.redirect("/");
 });
 
 app.get("/*", (request, response) => {
 response.redirect("/");
-});
+}); 
